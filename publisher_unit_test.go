a140c84c8d8b95ceadb12c0f68110b31af14533f
Denis Anfimov <denis.anfimov@netcracker.com>
2025-03-27 20:48:16 +0500

    backport loop back fix

--- publisher_unit_test.go
+++ publisher_unit_test.go
@@ -84,6 +84,24 @@ func TestNewPath403_OldPath_503(t *testing.T) {
 	assertions.Equal(1, oldPathAttempts)
 }
 
+func TestNewPath503_OldPath_503(t *testing.T) {
+	newPathAttempts := 0
+	assertions := require.New(t)
+
+	ts := createTestServer(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/v1/kv/bluegreen/test-namespace-origin/bgstate") {
+			newPathAttempts++
+			w.WriteHeader(http.StatusServiceUnavailable)
+		}
+	})
+	defer ts.Close()
+	DefaultPollingWaitTime = time.Second
+	DefaultTimeoutDuration = 500 * time.Millisecond
+	_, err := NewPublisher(context.Background(), ts.URL, testOriginNamespace, nil)
+	assertions.Error(err)
+	assertions.Equal(1, newPathAttempts)
+}
+
 func createTestServer(fn func(w http.ResponseWriter, r *http.Request)) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(fn))
 }
